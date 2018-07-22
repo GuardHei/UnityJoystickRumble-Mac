8106b56b4c822e4cd261efedf013ee7ec02f19c2
GuardHei <30316509+GuardHei@users.noreply.github.com>
2018-07-22 22:21:49 +0800

    Edit README

---
+++ MacXboxControllerRumbleUtil.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class MacXboxControllerRumbleUtil {
+
+	#region SDL2 Bridge
+
+	/*
+	 * This part of work is based on the project SDL2# - C# Wrapper for SDL2 by Ethan "flibitijibibo" Lee <flibitijibibo@flibitijibibo.com>
+	 * Copyright (c) 2013-2016 Ethan Lee.
+	 * https://github.com/flibitijibibo/SDL2-CS
+	 */
+
+	public const string nativeLibName = "SDL2";
+	public const uint SDL_INIT_HAPTIC = 0x00001000;
+
+	// todo customized rumble effects
+	
+	public const ushort SDL_HAPTIC_CONSTANT = 1 << 0;
+	public const ushort SDL_HAPTIC_SINE = 1 << 1;
+	public const ushort SDL_HAPTIC_LEFTRIGHT = 1 << 2;
+	public const ushort SDL_HAPTIC_TRIANGLE = 1 << 3;
+	public const ushort SDL_HAPTIC_SAWTOOTHUP = 1 << 4;
+	public const ushort SDL_HAPTIC_SAWTOOTHDOWN = 1 << 5;
+	public const ushort SDL_HAPTIC_SPRING = 1 << 7;
+	public const ushort SDL_HAPTIC_DAMPER = 1 << 8;
+	public const ushort SDL_HAPTIC_INERTIA = 1 << 9;
+	public const ushort SDL_HAPTIC_FRICTION = 1 << 10;
+	public const ushort SDL_HAPTIC_CUSTOM = 1 << 11;
+	public const ushort SDL_HAPTIC_GAIN = 1 << 12;
+	public const ushort SDL_HAPTIC_AUTOCENTER = 1 << 13;
+	public const ushort SDL_HAPTIC_STATUS = 1 << 14;
+	public const ushort SDL_HAPTIC_PAUSE = 1 << 15;
+
+	public const byte SDL_HAPTIC_POLAR = 0;
+	public const byte SDL_HAPTIC_CARTESIAN = 1;
+	public const byte SDL_HAPTIC_SPHERICAL = 2;
+
+	public const uint SDL_HAPTIC_INFINITY = 4292967295U;
+
+	// Operation State Int
+	public const int SDL_SUCCESS = 0;
+
+	// CPP BOOL
+	public static class SDL_BOOL {
+		public const int SDL_FALSE = 0;
+		public const int SDL_TRUE = 1;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct SDL_HapticDirection {
+		public byte type;
+		public fixed int dir[3];
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDL_HapticConstant {
+		public ushort type;
+		public SDL_HapticDirection direction;
+
+		public uint length;
+		public ushort delay;
+
+		public ushort button;
+		public ushort interval;
+
+		public short level;
+
+		public ushort attack_length;
+		public ushort attack_level;
+		public ushort fade_length;
+		public ushort fade_level;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDL_HapticPeriodic {
+		public ushort type;
+		public SDL_HapticDirection direction;
+
+		public uint length;
+		public ushort delay;
+
+		public ushort button;
+		public ushort interval;
+
+		public ushort period;
+		public short magnitude;
+		public short offset;
+		public ushort phase;
+
+		public ushort attack_length;
+		public ushort attack_level;
+		public ushort fade_length;
+		public ushort fade_level;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public unsafe struct SDL_HapticCondition {
+		public ushort type;
+		public SDL_HapticDirection direction;
+
+		public uint length;
+		public ushort delay;
+
+		public ushort button;
+		public ushort interval;
+
+		public fixed ushort right_sat[3];
+		public fixed ushort left_sat[3];
+		public fixed short right_coeff[3];
+		public fixed short left_coeff[3];
+		public fixed ushort deadband[3];
+		public fixed short center[3];
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDL_HapticRamp {
+		public ushort type;
+		public SDL_HapticDirection direction;
+
+		public uint length;
+		public ushort delay;
+
+		public ushort button;
+		public ushort interval;
+
+		public short start;
+		public short end;
+
+		public ushort attack_length;
+		public ushort attack_level;
+		public ushort fade_length;
+		public ushort fade_level;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDL_HapticLeftRight {
+		public ushort type;
+
+		public uint length;
+
+		public ushort large_magnitude;
+		public ushort small_magnitude;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDL_HapticCustom {
+		public ushort type;
+		public SDL_HapticDirection direction;
+
+		public uint length;
+		public ushort delay;
+
+		public ushort button;
+		public ushort interval;
+
+		public byte channels;
+		public ushort period;
+		public ushort samples;
+		public IntPtr data;
+
+		public ushort attack_length;
+		public ushort attack_level;
+		public ushort fade_length;
+		public ushort fade_level;
+	}
+
+	[StructLayout(LayoutKind.Explicit)]
+	public struct SDL_HapticEffect {
+		[FieldOffset(0)] public ushort type;
+		[FieldOffset(0)] public SDL_HapticConstant constant;
+		[FieldOffset(0)] public SDL_HapticPeriodic periodic;
+		[FieldOffset(0)] public SDL_HapticCondition condition;
+		[FieldOffset(0)] public SDL_HapticRamp ramp;
+		[FieldOffset(0)] public SDL_HapticLeftRight leftright;
+		[FieldOffset(0)] public SDL_HapticCustom custom;
+	}
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_Init(uint flags);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void SDL_Quit();
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern uint SDL_WasInit(uint flags);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void SDL_HapticClose(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void SDL_HapticDestroyEffect(IntPtr haptic, int effect);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticEffectSupported(IntPtr haptic, ref SDL_HapticEffect effect);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticGetEffectStatus(IntPtr haptic, int effect);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticIndex(IntPtr haptic);
+
+	[DllImport(nativeLibName, EntryPoint = "SDL_HapticName", CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr INTERNAL_SDL_HapticName(int device_index);
+
+	private static string SDL_HapticName(int device_index) {
+		return UTF8_ToManaged(INTERNAL_SDL_HapticName(device_index));
+	}
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticNewEffect(IntPtr haptic, ref SDL_HapticEffect effect);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticNumAxes(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticNumEffects(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticNumEffectsPlaying(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr SDL_HapticOpen(int device_index);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticOpened(int device_index);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticPause(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern uint SDL_HapticQuery(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticRumbleInit(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticRumblePlay(IntPtr haptic, float strength, uint length);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticRumbleStop(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticRumbleSupported(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticRunEffect(IntPtr haptic, int effect, uint iterations);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticSetAutocenter(IntPtr haptic, int autocenter);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticSetGain(IntPtr haptic, int gain);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticStopAll(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticStopEffect(IntPtr haptic, int effect);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticUnpause(IntPtr haptic);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_HapticUpdateEffect(IntPtr haptic, int effect, ref SDL_HapticEffect data);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int SDL_NumHaptics();
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr SDL_malloc(IntPtr size);
+
+	[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void SDL_free(IntPtr memblock);
+
+	private static unsafe string UTF8_ToManaged(IntPtr s, bool freePtr = false) {
+		if (s == IntPtr.Zero) return null;
+		byte* ptr = (byte*) s;
+		while (*ptr != 0) {
+			ptr++;
+		}
+
+		string result = System.Text.Encoding.UTF8.GetString((byte*) s, (int) (ptr - (byte*) s));
+		if (freePtr) SDL_free(s);
+		return result;
+	}
+
+	#endregion
+
+	public static bool IsInitiated => SDL_WasInit(SDL_INIT_HAPTIC) == SDL_BOOL.SDL_TRUE;
+	public static int DeviceNum => SDL_NumHaptics();
+
+	public static string[] DeviceNames {
+		get {
+			int deviceNum = DeviceNum;
+			if (deviceNum < 0) {
+				return null;
+			}
+
+			string[] deviceNames = new string[deviceNum];
+			for (int i = 0; i < deviceNum; i++) {
+				deviceNames[i] = SDL_HapticName(i);
+			}
+
+			return deviceNames;
+		}
+	}
+
+	private static Dictionary<int, IntPtr> availableDevices = new Dictionary<int, IntPtr>();
+
+	static MacXboxControllerRumbleUtil() {
+		SDL_Init(SDL_INIT_HAPTIC);
+	}
+
+	public static bool Rumble(int deviceIndex, float strength, int milliseconds) {
+		if (!CheckAndInitDevice(deviceIndex)) return false;
+		if (milliseconds < 0) return false;
+		IntPtr device = availableDevices[deviceIndex];
+		return SDL_HapticRumblePlay(device, strength, (uint) milliseconds) == SDL_SUCCESS;
+	}
+
+	public static bool Rumble(float strength, int milliseconds) {
+		return Rumble(0, strength, milliseconds);
+	}
+
+	public static bool Rumble(int milliseconds = 1000) {
+		return Rumble(0, 1, milliseconds);
+	}
+
+	public static void StopRumble(int deviceIndex) {
+		if (availableDevices.ContainsKey(deviceIndex)) return;
+		SDL_HapticRumbleStop(availableDevices[deviceIndex]);
+	}
+
+	public static void StopRumbleAll() {
+		foreach (var pair in availableDevices) {
+			SDL_HapticRumbleStop(pair.Value);
+		}
+	}
+
+	private static bool CheckAndInitDevice(int deviceIndex) {
+		if (availableDevices.ContainsKey(deviceIndex)) return true;
+		if (deviceIndex >= DeviceNum) return false;
+		IntPtr ptr = SDL_HapticOpen(deviceIndex);
+		if (SDL_HapticRumbleSupported(ptr) == SDL_BOOL.SDL_FALSE) return false;
+		if (SDL_HapticRumbleInit(ptr) != SDL_SUCCESS) return false;
+
+		availableDevices[deviceIndex] = ptr;
+		Debug.Log("Register Device [" + deviceIndex + "] " + SDL_HapticName(deviceIndex) + " !");
+		return true;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,102 @@
 # UnityJoystickRumble-Mac
-Scripts and plugins to enable rumble functions of joysticks on Mac
+
+## Purpose
+This project implements the rumble / force feedback function of the joysticks on Mac.
+
+## Origin
+As an indie developer, I've been long suffering from lacking of official support on Mac platform. To vibrate the joysticks on Windows, XInput and InControl can do a good job, but still, there is no solution for Mac. Therefore, I started this project.
+
+## How I Make it
+I have to admit my implementation is quite weird. Baiscally, I imported the framework from another game engine __SDL-2__, based on C, and tried to access it from C#. Therefore, unexpected problems or bugs might appear.
+
+The idea of using __SDL-2__ came from another popular Mac XBox Controller driver repo [360Controller](https://github.com/360Controller/360Controller) (Also works with Xbox One Controller).
+
+I didn't really make a native plugin but found a solution. To wrap up the C interfaces into C#, I checked this repo [SDL2-CS](https://github.com/flibitijibibo/SDL2-CS) and extracted and edited stuffs to get everything done and explicit.
+
+## Requirements
+1. The aimed controller is wired connected via [this driver](https://github.com/360Controller/360Controller) (The repo above). Other drivers might still work because __SDL-2__ lib is pretty platform universal.
+
+2. This repo
+
+## Installation
+1. To import a native plugin into Unity for mac, you need a *.bundle* file. This file is located in the __Plugins__ folder of this repo. However, you can also make it yourself. Download the installation file *.dmg* from __SDL-2__'s official site (Make sure it is Development Libraries instead of Runtime Binaries). Then, open the *.dmg* file, you will see the framework file __SDL2.framework__ inside. Just change the suffix *.framework* to *.bundle* and drag it into the __Plugins__ folder of your Unity project. This is because *.framework* is a subform of bundle file on Mac. Unity doesn't recognize framework files. However, during runtime, Mac will swallow it the same as *.bundle*.
+
+2. Make sure this plugin is targeted only for Editor (if the development environment is Mac) and OSX 64 Standalones. Note that this plugin doesn't work with x86 system (32 bit).
+
+3. Import the C# script into your Unity project. You can change the script implementation if you want. Just make it your self.
+
+## How To Use It
+To make this tool script simple, I even don't add an extra namespace to it. Just direct call from the static class `MacXboxControllerRumbleUtil`.
+
+### Rumble
+To make a rumble, directly add this line of code into your script:
+
+```csharp
+MacXboxControllerRumbleUtil.Rumble();
+```
+
+The your joystick should rumble for one second. The overrides of the methods are listed below:
+
+```csharp
+public static bool Rumble(int deviceIndex, float strength, int milliseconds);
+public static bool Rumble(float strength, int milliseconds);
+public static bool Rumble(int milliseconds = 1000);
+```
+
+1. The first parameter device index is the index of you game controllers. Most of the time should be 0 (the first recognizd device). This lib doesn't recognize normal keyboards and mouses. Actually the 2nd and the 3rd overrides just set the default deviceIndex to 0.
+
+2. The second paramenter strength is the magnitude of rumble between 0 to 1. Default is 1.
+
+3. The third parameter milliseconds is the rumble duration in milliseconds.
+
+4. The return bool value represents whether the execuation is successful (true) or failed (false).
+
+### Stop Rumble
+
+```csharp
+public static void StopRumble(int deviceIndex);
+public static void StopRumbleAll();
+```
+
+### Other Attributes
+1. Whether the __SDL-2__ lib has been successfully initiated:
+
+```csharp
+public static bool IsInitiated;
+```
+
+The initiation should be done automatically in the static constructor. Don't do it manually.
+
+2. The number of haptic devices connected:
+
+```csharp
+public static int DeviceNum;
+```
+
+This is an accessor property.
+
+3. The names of haptic devices connected:
+
+```csharp
+public static string[] DeviceNames;
+```
+
+This is an accessor property.
+
+## Customize
+
+I haven't finish every part of the rumble function yet. If you check my script, you can see many public structs and static extern methods modified private. This is because the customized effect rumble function is still under development, including Sine Rumble, Spring Rumble...... But you can try to make it your self.
+
+To customize your own rumble effects, you first have to get an `IntPtr` instance which represents a device. Call the bridge function `SDL_HapticOpen(int device_index)` will return an `IntPrt` instance points to the device at `device_index`.
+
+Then, `new SDL_HapticEffect()` to create the data of your customized effect. I won't go into details of each field, because I am still studying this. But basically, consider the rumble as the movement of waves. Setting up the wave type, direction and other attributes will give your various forms of force feedback. If you want to learn more, you can check the [official __SDL-2__ api](http://wiki.libsdl.org/CategoryForceFeedback) (written in C).
+
+After that, use method `SDL_HapticNewEffect(IntPtr haptic, ref SDL_HapticEffect effect)` to register your customized effect into the system. The return value is int type. Smaller than 0 means a failure registration. Otherwise, the value represent the effect ID. Store this value because it is useful in the next step.
+
+To run a registerd effect, you need method `SDL_HapticRunEffect(IntPtr haptic, int effect, uint iterations)` to make the effect work. Note that the second parameter is the id of the effect you want to call, and iterations means the number of repeatings.
+
+After all, use `SDL_HapticDestroyEffect(IntPtr haptic, int effect)` to destroy the used effects. And then call `SDL_HapticClose(IntPtr haptic)` to close the module. Meanwhile, if you call `SDL_HapticClose(IntPtr haptic)` directly, the registered effects will be automatically destroyed.
+
+I don't recommand you to close the device everytime after rumble, just destroy the effect is enough.
+
+PS: There might be some unsafe codes of some specific effect type, using pointers. Make sure you have change the settings of "allow unsafe code" in Unity Player Settings.
